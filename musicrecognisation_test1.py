54b64ab7d21680766b88025da7ce70ac71ed7485
Pranav <pranavnat24@gmail.com>
2019-01-05 19:10:46 +0530

    Extract required data from nested dictionary

--- musicrecognisation_test1.py
+++ musicrecognisation_test1.py
@@ -1,4 +1,5 @@
 from acrcloud.recognizer import ACRCloudRecognizer
+import json
 
 config = {
     'host': 'eu-west-1.api.acrcloud.com',
@@ -8,7 +9,28 @@ config = {
     'timeout': 10
 }
 
+
 acrcloud = ACRCloudRecognizer(config)
 
-print(acrcloud.recognize_by_file('/home/pranav/Desktop/myfinalProject/Test/csr.mp3', 0))
+# Convert output 'str' from function to 'dict'
+S = json.loads(acrcloud.recognize_by_file('/home/pranav/Desktop/myfinalProject/Test/csr.mp3', 0))
+
+# Get required items from 'dict' and convert to 'list'
+C = S['metadata']['music']
+
+# Pop objects from list
+A = C.pop()
+
+# Get artist name from metadata
+V = A['external_metadata']['deezer']
+O = V['artists'].pop()
+
+print("Artist name: ", O['name'])
+print("Album name: ", A['album']['name'])
+print("Release date: ", A['release_date'])
+print("Title:", A['title'])
+print("Label: ", A['label'])
+print("Duration: ", A['duration_ms'], "ms")
+
+
 
