6d4a4e65fcbecb6a17f72205ddb4d673f7858d6b
Pranav <pranavnat24@gmail.com>
2019-02-09 22:25:48 +0530

    GUI changes

--- GUI.py
+++ GUI.py
@@ -22,3 +22,49 @@ try:
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(993, 692)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.pbLevel = QtGui.QProgressBar(self.centralwidget)
+        self.pbLevel.setMaximum(1000)
+        self.pbLevel.setProperty("value", 123)
+        self.pbLevel.setTextVisible(False)
+        self.pbLevel.setOrientation(QtCore.Qt.Vertical)
+        self.pbLevel.setObjectName(_fromUtf8("pbLevel"))
+        self.horizontalLayout.addWidget(self.pbLevel)
+        self.frame = QtGui.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtGui.QFrame.NoFrame)
+        self.frame.setFrameShadow(QtGui.QFrame.Plain)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.frame)
+        self.verticalLayout.setMargin(0)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label = QtGui.QLabel(self.frame)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        self.grFFT = PlotWidget(self.frame)
+        self.grFFT.setObjectName(_fromUtf8("grFFT"))
+        self.verticalLayout.addWidget(self.grFFT)
+        self.label_2 = QtGui.QLabel(self.frame)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.verticalLayout.addWidget(self.label_2)
+        self.grPCM = PlotWidget(self.frame)
+        self.grPCM.setObjectName(_fromUtf8("grPCM"))
+        self.verticalLayout.addWidget(self.grPCM)
+        self.horizontalLayout.addWidget(self.frame)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.label.setText(_translate("MainWindow", "frequency data (FFT):", None))
+        self.label_2.setText(_translate("MainWindow", "raw data (PCM):", None))
