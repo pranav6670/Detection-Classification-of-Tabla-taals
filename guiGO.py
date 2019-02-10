ad296b044ceb9a55af29d0faa45880cbeca5d22a
Pranav <pranavnat24@gmail.com>
2019-02-10 21:55:52 +0530

    GUI changes

---
+++ guiGO.py
@@ -0,0 +1,26 @@
+import GUI
+from PyQt4 import QtGui, QtCore
+
+import numpy as np
+import sys
+import time
+import pyqtgraph
+
+
+class ExampleApp(QtGui.QMainWindow, GUI.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        pyqtgraph.setConfigOption('background', 'w')
+        super(ExampleApp, self).__init__(parent)
+        self.setupUi(self)
+        self.fftPlot.plotItem.showGrid(True, True, 0.7)
+        self.rawAudio.plotItem.showGrid(True, True, 0.7)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    form.update() #start with something
+    app.exec_()
+    print("DONE")
