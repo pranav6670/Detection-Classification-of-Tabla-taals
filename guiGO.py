e8a9558e5e2f4f656cdf6571c0009d7763cc263d
Pranav <pranavnat24@gmail.com>
2019-02-12 01:28:01 +0530

    Created audio backend and GUI changes

--- guiGO.py
+++ guiGO.py
@@ -1,6 +1,6 @@
 import GUI
 from PyQt4 import QtGui, QtCore
-
+import audioBackend
 import numpy as np
 import sys
 import time
@@ -15,6 +15,28 @@ class ExampleApp(QtGui.QMainWindow, GUI.Ui_MainWindow):
         self.setupUi(self)
         self.raw.plotItem.showGrid(True, True, 0.7)
         self.FFT.plotItem.showGrid(True, True, 0.7)
+        self.hear = audioBackend.PNstream(rate=44100, updatesPerSecond=20)
+        self.hear.stream_start()
+        self.maxraw = 0
+        self.maxFFT = 0
+
+    def update(self):
+        if not self.hear.data is None and not self.hear.fft is None:
+
+            maxraw = np.max(np.abs(self.hear.data))
+            if maxraw > self.maxraw:
+                self.maxraw = maxraw
+                self.raw.plotItem.setRange(yRange=[-maxraw, maxraw])
+
+            if np.max(self.hear.fft) > self.maxFFT:
+                self.maxFFT = np.max(np.abs(self.hear.fft))
+                self.FFT.plotItem.setRange(yRange=[0, 1])
+
+            pen = pyqtgraph.mkPen(color='r')
+            self.raw.plot(self.hear.datax, self.hear.data, pen=pen, clear=True)
+            pen = pyqtgraph.mkPen(color='r')
+            self.grFFT.plot(self.hear.fftx, self.hear.fft/self.maxFFT, pen=pen, clear=True)
+        QtCore.QTimer.singleShot(1, self.update)  # QUICKLY repeat
 
 
 if __name__ == "__main__":
