598d1bab2be32ad721b99dce043c943525afbde3
Pranav <pranavnat24@gmail.com>
2019-02-12 00:12:13 +0530

    GUI changes

--- GUI.py
+++ GUI.py
@@ -1,13 +1,13 @@
+
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'file.ui'
+# Form implementation generated from reading ui file 'test.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-from pyqtgraph import PlotWidget
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -24,50 +24,45 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(object):
-
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(993, 692)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.pbLevel = QtGui.QProgressBar(self.centralwidget)
-        self.pbLevel.setMaximum(1000)
-        self.pbLevel.setProperty("value", 123)
-        self.pbLevel.setTextVisible(False)
-        self.pbLevel.setOrientation(QtCore.Qt.Vertical)
-        self.pbLevel.setObjectName(_fromUtf8("pbLevel"))
-        self.horizontalLayout.addWidget(self.pbLevel)
-        self.frame = QtGui.QFrame(self.centralwidget)
-        self.frame.setFrameShape(QtGui.QFrame.NoFrame)
-        self.frame.setFrameShadow(QtGui.QFrame.Plain)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.frame)
-        self.verticalLayout.setMargin(0)
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label = QtGui.QLabel(self.frame)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.verticalLayout.addWidget(self.label)
-        self.grFFT = PlotWidget(self.frame)
-        self.grFFT.setObjectName(_fromUtf8("grFFT"))
-        self.verticalLayout.addWidget(self.grFFT)
-        self.label_2 = QtGui.QLabel(self.frame)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.verticalLayout.addWidget(self.label_2)
-        self.grPCM = PlotWidget(self.frame)
-        self.grPCM.setObjectName(_fromUtf8("grPCM"))
-        self.verticalLayout.addWidget(self.grPCM)
-        self.horizontalLayout.addWidget(self.frame)
+        self.rawAudio = QtGui.QFrame(self.centralwidget)
+        self.rawAudio.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.rawAudio.setFrameShadow(QtGui.QFrame.Raised)
+        self.rawAudio.setObjectName(_fromUtf8("rawAudio"))
+        self.verticalLayout.addWidget(self.rawAudio)
+        self.FFT = QtGui.QFrame(self.centralwidget)
+        self.FFT.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.FFT.setFrameShadow(QtGui.QFrame.Raised)
+        self.FFT.setObjectName(_fromUtf8("FFT"))
+        self.verticalLayout.addWidget(self.FFT)
+        self.start = QtGui.QPushButton(self.centralwidget)
+        self.start.setObjectName(_fromUtf8("start"))
+        self.verticalLayout.addWidget(self.start)
+        self.stop = QtGui.QPushButton(self.centralwidget)
+        self.stop.setObjectName(_fromUtf8("stop"))
+        self.verticalLayout.addWidget(self.stop)
         MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "frequency data (FFT):", None))
-        self.label_2.setText(_translate("MainWindow", "raw data (PCM):", None))
+        self.start.setText(_translate("MainWindow", "Start!!", None))
+        self.stop.setText(_translate("MainWindow", "Stop!!", None))
 
 
 if __name__ == "__main__":
