33971d7bd9d912bde3bf8fddab2831649b4d5dac
Pranav <pranavnat24@gmail.com>
2019-02-12 01:32:20 +0530

    Created audio backend and GUI changes

--- audioBackend.py
+++ audioBackend.py
@@ -14,85 +14,28 @@ def getFFT(data, rate):
 
 class PNstream():
 
-    def __init__(self, device=None, rate=None, updatesPerSecond=10):
+    def __init__(self):
         self.p = pyaudio.PyAudio()
-        self.chunk = 4096  # gets replaced automatically
-        self.updatesPerSecond = updatesPerSecond
-        self.chunksRead = 0
-        self.device = device
-        self.rate = rate
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 44100
+        self.CHUNK = 1024 * 4
+        self.DEVICE = 2
 
-    ### SYSTEM TESTS
+        # Open the stream
+        #####################################
+        self.stream = self.p.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            output=True,
+            frames_per_buffer=self.CHUNK,
+            input_device_index=self.DEVICE
+        )
+        ######################################
 
-    def valid_low_rate(self, device):
-        """set the rate to the lowest supported audio rate."""
-        for testrate in [44100]:
-            if self.valid_test(device, testrate):
-                return testrate
-        print("SOMETHING'S WRONG! I can't figure out how to use DEV", device)
-        return None
 
-    def valid_test(self, device, rate=44100):
-        """given a device ID and a rate, return TRUE/False if it's valid."""
-        try:
-            self.info = self.p.get_device_info_by_index(device)
-            if not self.info["maxInputChannels"] > 0:
-                return False
-            stream = self.p.open(format=pyaudio.paInt16, channels=1,
-                                 input_device_index=device, frames_per_buffer=self.chunk,
-                                 rate=int(self.info["defaultSampleRate"]), input=True)
-            stream.close()
-            return True
-        except:
-            return False
-
-    def valid_input_devices(self):
-        """
-        See which devices can be opened for microphone input.
-        call this when no PyAudio object is loaded.
-        """
-        mics = []
-        for device in range(self.p.get_device_count()):
-            if self.valid_test(device):
-                mics.append(device)
-        if len(mics) == 0:
-            print("no microphone devices found!")
-        else:
-            print("found %d microphone devices: %s" % (len(mics), mics))
-        return mics
-
-        ### SETUP AND SHUTDOWN
-
-    def initiate(self):
-        """run this after changing settings (like rate) before recording"""
-        if self.device is None:
-            self.device = self.valid_input_devices()[0]  # pick the first one
-
-        if self.rate is None:
-            self.rate = self.valid_low_rate(self.device)
-
-        self.chunk = int(self.rate / self.updatesPerSecond)  # hold one tenth of a second in memory
-
-        if not self.valid_test( self.device, self.rate):
-            print("guessing a valid microphone device/rate...")
-            self.device = self.valid_input_devices()[0]  # pick the first one
-            self.rate = self.valid_low_rate(self.device)
-
-        self.datax = np.arange(self.chunk) / float(self.rate)
-        msg = 'recording from "%s" ' % self.info["name"]
-        msg += '(device %d) ' % self.device
-        msg += 'at %d Hz' % self.rate
-        print(msg)
-
-    def close(self):
-        """gently detach from things."""
-        print(" -- sending stream termination command...")
-        self.keepRecording = False  # the threads should self-close
-
-        while (self.t.isAlive()):  # wait for all threads to close
-            time.sleep(.1)
-        self.stream.stop_stream()
-        self.p.terminate()
 
     ### STREAM HANDLING
 
