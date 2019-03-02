6e558ebfbe1e87443dc26ef19f1fc86578ccf977
Pranav <pranavnat24@gmail.com>
2019-03-02 12:22:47 +0530

    Improving GUI

--- GUI.py
+++ GUI.py
@@ -1,80 +1,52 @@
+
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'gui.ui'
+# Form implementation generated from reading ui file '3way.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.verticalLayout.addWidget(self.label)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
         self.raw = PlotWidget(self.centralwidget)
-        self.raw.setObjectName(_fromUtf8("raw"))
-        self.verticalLayout.addWidget(self.raw)
-        self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.verticalLayout.addWidget(self.label_2)
-        self.FFT = PlotWidget(self.centralwidget)
-        self.FFT.setObjectName(_fromUtf8("FFT"))
-        self.verticalLayout.addWidget(self.FFT)
-        self.start = QtGui.QPushButton(self.centralwidget)
-        self.start.setObjectName(_fromUtf8("start"))
-        self.verticalLayout.addWidget(self.start)
-        self.stop = QtGui.QPushButton(self.centralwidget)
-        self.stop.setObjectName(_fromUtf8("stop"))
-        self.verticalLayout.addWidget(self.stop)
+        self.raw.setGeometry(QtCore.QRect(10, 20, 781, 192))
+        self.raw.setObjectName("raw")
+        self.fft = PlotWidget(self.centralwidget)
+        self.fft.setGeometry(QtCore.QRect(10, 220, 781, 192))
+        self.fft.setObjectName("fft")
+        self.spec = PlotWidget(self.centralwidget)
+        self.spec.setGeometry(QtCore.QRect(10, 430, 781, 131))
+        self.spec.setObjectName("spec")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "Raw Audio", None))
-        self.label_2.setText(_translate("MainWindow", "FFT ", None))
-        self.start.setText(_translate("MainWindow", "Start!!", None))
-        self.stop.setText(_translate("MainWindow", "Stop!!", None))
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
 
 from pyqtgraph import PlotWidget
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
