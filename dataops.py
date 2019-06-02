ee8b8b07695f9fbe4291b8c34f34ed1a14850081
Pranav <pranavnat24@gmail.com>
2019-06-02 19:55:45 +0530

    Data loading and Cleaning

--- dataops.py
+++ dataops.py
@@ -12,7 +12,7 @@ def plot_signals(signals):
     fig.suptitle('Time Series', size=16)
     i = 0
     for x in range(2):
-        for y in range(5):
+        for y in range(4):
             axes[x,y].set_title(list(signals.keys())[i])
             axes[x,y].plot(list(signals.values())[i])
             axes[x,y].get_xaxis().set_visible(False)
@@ -25,7 +25,7 @@ def plot_fft(fft):
     fig.suptitle('Fourier Transforms', size=16)
     i = 0
     for x in range(2):
-        for y in range(5):
+        for y in range(4):
             data = list(fft.values())[i]
             Y, freq = data[0], data[1]
             axes[x,y].set_title(list(fft.keys())[i])
@@ -40,7 +40,7 @@ def plot_fbank(fbank):
     fig.suptitle('Filter Bank Coefficients', size=16)
     i = 0
     for x in range(2):
-        for y in range(5):
+        for y in range(4):
             axes[x,y].set_title(list(fbank.keys())[i])
             axes[x,y].imshow(list(fbank.values())[i],
                     cmap='hot', interpolation='nearest')
@@ -54,10 +54,12 @@ def plot_mfccs(mfccs):
     fig.suptitle('Mel Frequency Cepstrum Coefficients', size=16)
     i = 0
     for x in range(2):
-        for y in range(5):
+        for y in range(4):
             axes[x,y].set_title(list(mfccs.keys())[i])
             axes[x,y].imshow(list(mfccs.values())[i],
                     cmap='hot', interpolation='nearest')
             axes[x,y].get_xaxis().set_visible(False)
             axes[x,y].get_yaxis().set_visible(False)
             i += 1
+
+
