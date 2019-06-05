44338b67e59294fb0ab02a9fc68159a82989da87
Pranav <pranavnat24@gmail.com>
2019-06-05 21:34:33 +0530

    Modelling and Training

--- dataops.py
+++ dataops.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import librosa
 from python_speech_features import mfcc, logfbank
-plt.style.use('ggplot')
+plt.style.use('seaborn-pastel')
 
 def plot_signals(signals):
     fig, axes = plt.subplots(nrows=2, ncols=4, sharex=False,
@@ -110,7 +110,7 @@ for c in classes:
     wav_file = df[df.label == c].iloc[0, 0]
     signal, rate = librosa.load('wavfiles/'+wav_file, sr=44100)
     mask = envelope(signal, rate, threshold=0.0005)
-    # signal = signal[mask]
+    signal = signal[mask]
     signals[c] = signal
     fft[c] = calc_fft(signal, rate)
     bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
